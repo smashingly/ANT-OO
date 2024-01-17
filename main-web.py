3e375c64d8a1651b4be7a6a133558040dad0ce56
ash <8407439+smashingly@users.noreply.github.com>
2024-01-17 12:01:07 +1100

    Added requirements.txt, created main-web.py to experiment with Flask implementation (basic 'hello world' at this stage)

---
+++ main-web.py
@@ -0,0 +1,39 @@
+from ant import TestEngine, ManualImportHandler, StdoutExportHandler, CsvImportHandler
+import logging
+from flask import Flask
+
+"""################################### START OF LOGGER CONFIGURATION ###################################"""
+# Create a custom logger
+logger = logging.getLogger("ant")
+logger.setLevel(logging.DEBUG)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler("ant.log")
+c_handler.setLevel(logging.ERROR)
+f_handler.setLevel(logging.DEBUG)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter("%(levelname)s: %(message)s")
+f_format = logging.Formatter(fmt="%(asctime)s.%(msecs)03d - %(levelname)08s: %(funcName)s: %(message)s",
+                             datefmt="%Y-%m-%d %H:%M:%S")
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+"""#################################### END OF LOGGER CONFIGURATION ####################################"""
+
+# test_engine = TestEngine(ManualImportHandler(), StdoutExportHandler())
+# test_engine = TestEngine(CsvImportHandler("tests.csv"), StdoutExportHandler())
+# test_engine.run_tests()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
